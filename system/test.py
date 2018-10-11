bec329afc92c044c7485191d4ca754085c3790c6
Rafael Teodosio <rafzteodosio@gmail.com>
2018-10-11 18:50:42 -0300

    update views; refs: fga-eps-mds/2018.2-PDF2CASH#103
    Co-Authored-By: Lucas Gomes Silva <lucas.gomesgs0@gmail.com>
    Co-Authored-By: Bruno Matias <leguenta@hotmail.com>

--- system/test.py
+++ system/test.py
@@ -1,11 +1,56 @@
 from django.test import TestCase
+import json
 
 
 class SystemTest(TestCase):
 
-    def test_post(self):
+    def test_post_1(self):
         url = '/api/system/pdftoinvoice/'
-        file1 = open("system/urls.py" , 'rb')
-        file2 = open("system/views.py" , 'rb')
-        response = self.client.post(url,{'files': [file1, file2]}, format='multipart')
+        file = open("system/test.zip" , 'rb')
+        response = self.client.post(url,{'file': file}, format='multipart')
         self.assertEqual(response.status_code, 200)
+
+    def test_post_2(self):
+        url = '/api/system/bi/'
+        file = open("system/test.zip" , 'rb')
+        response = self.client.post(url,{'file': file}, format='multipart')
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_3(self):
+        url = '/api/system/frontend/'
+        file = open("system/test.zip" , 'rb')
+        response = self.client.post(url,{'file': file}, format='multipart')
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_4(self):
+        url = '/api/system/management/'
+        file = open("system/test.zip" , 'rb')
+        response = self.client.post(url,{'file': file}, format='multipart')
+        self.assertEqual(response.status_code, 200)
+
+    def test_version(self):
+        url = '/api/system/management/'
+        file = open("system/test.zip" , 'rb')
+        response = self.client.post(url,{'file': file}, format='multipart')
+        self.assertEqual(json.loads(response.content)[1]['version'], 1)
+        url = '/api/system/frontend/'
+        file = open("system/test.zip" , 'rb')
+        response = self.client.post(url,{'file': file}, format='multipart')
+        self.assertEqual(json.loads(response.content)[1]['version'], 2)
+
+    def test_get_4(self):
+        url = '/api/system/pdftoinvoice/'
+        file = open("system/test.zip" , 'rb')
+        response = self.client.post(url,{'file': file}, format='multipart')
+        url = '/api/system/bi/'
+        file = open("system/test.zip" , 'rb')
+        response = self.client.post(url,{'file': file}, format='multipart')
+        url = '/api/system/frontend/'
+        file = open("system/test.zip" , 'rb')
+        response = self.client.post(url,{'file': file}, format='multipart')
+        url = '/api/system/management/'
+        file = open("system/test.zip" , 'rb')
+        response = self.client.post(url,{'file': file}, format='multipart')
+        url = '/api/system/get/'
+        response = self.client.get(url)
+        self.assertNotEquals(response.content, b'')
